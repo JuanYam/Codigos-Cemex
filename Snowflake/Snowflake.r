625c4f0942026a9d835817049e6ab56ecc494902
JuanYam <54381681+JuanYam@users.noreply.github.com>
2023-01-04 13:54:28 -0600

    Remisiones

--- Snowflake/Snowflake.r
+++ Snowflake/Snowflake.r
@@ -1,19 +1,32 @@
-library(tabulizer)
-library(lubridate)
 library(openxlsx)
-library(pdftools)
 library(dplyr)
 library(readxl)
-library(stringr)
 
-DELIV = read_excel("C:/Users/yamli/OneDrive/Documentos/CEMEX/Snowflake/DELIV.xlsx")
+DELIV = read_excel("C:/Users/s-mxeocmexico/OneDrive - CEMEX/Documentos/GitHub/Codigos-Cemex/Snowflake/DELIV2.xlsx")
+DSERV = read_excel("C:/Users/s-mxeocmexico/OneDrive - CEMEX/Documentos/GitHub/Codigos-Cemex/Snowflake/DSERV.xlsx")
+CALL = read_excel("C:/Users/s-mxeocmexico/OneDrive - CEMEX/Documentos/GitHub/Codigos-Cemex/Snowflake/CALL.xlsx")
+
 
 DELIV <- DELIV[!duplicated(DELIV), ]
+DSERV <- DSERV[!duplicated(DSERV), ]
+
+length(unique(DELIV$Entrega))
+
+length(unique(DSERV$`Orden de Venta`))
+
+
+a <- subset(DSERV, DSERV$`Orden de Venta` == 8013709394 )
+
+b <- subset(DELIV, DELIV$`Documento de Venta` == 8013709394 )
 
 
 
 
-DELIV$key <- paste(DELIV$`Documento de Venta`,DELIV$`Unld Final Hr`, DELIV$`Unld Initial Hr`, DELIV$`Fin real (hora)`, DELIV$Entrega, DELIV$`Cantidad entrega`,DELIV$`In Site Hr`, DELIV$`Nombre 1`,DELIV$`Hora TJST`, DELIV$`Hora TOPL`, DELIV$`Nombre de la Obra`)
+DELIV$key <- paste(DELIV$Entrega)
+
+length(unique(DELIV$key))
+
+DELIV$key <- paste(DELIV$`Documento de Venta`,DELIV$`Unld Final Hr`, DELIV$`Unld Initial Hr`, DELIV$`Fin real (hora)`, DELIV$Entrega, DELIV$`Cantidad entrega`,DELIV$`In Site Hr`, DELIV$`Nombre 1`,DELIV$`Hora TJST`, DELIV$`Hora TOPL`)
 
 length(unique(DELIV$key))
 
@@ -28,3 +41,7 @@ for( r in 1:length(i)){
   }
 }
 
+
+a <- subset(DELIV, DELIV$Entrega == 224198962)
+
+b <- subset(DSERV, DSERV$Entrega == 224198962)
