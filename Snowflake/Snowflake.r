90302f9f991d5bd154873537db78f7ffc535f387
JuanYam <54381681+JuanYam@users.noreply.github.com>
2023-01-05 14:39:40 -0600

    Bases de datos para verificar las remisiones repetidas

--- Snowflake/Snowflake.r
+++ Snowflake/Snowflake.r
@@ -2,13 +2,43 @@ library(openxlsx)
 library(dplyr)
 library(readxl)
 
-DELIV = read_excel("C:/Users/s-mxeocmexico/OneDrive - CEMEX/Documentos/GitHub/Codigos-Cemex/Snowflake/DELIV2.xlsx")
-DSERV = read_excel("C:/Users/s-mxeocmexico/OneDrive - CEMEX/Documentos/GitHub/Codigos-Cemex/Snowflake/DSERV.xlsx")
-CALL = read_excel("C:/Users/s-mxeocmexico/OneDrive - CEMEX/Documentos/GitHub/Codigos-Cemex/Snowflake/CALL.xlsx")
-
+DELIV = read_excel("C:/Users/E-JAYAM/Documents/GitHub/Codigos-Cemex/Snowflake/DELIV2.xlsx")
+DSERV = read_excel("C:/Users/E-JAYAM/Documents/GitHub/Codigos-Cemex/Snowflake/DSERV.xlsx")
+CALL = read_excel("C:/Users/E-JAYAM/Documents/GitHub/Codigos-Cemex/Snowflake/CALL.xlsx")
+Plantas <- read_excel("C:/Users/E-JAYAM/OneDrive - CEMEX/BD PBI EXOP/Tablas de dimensiones/Base Plantas.xlsx")
 
 DELIV <- DELIV[!duplicated(DELIV), ]
 DSERV <- DSERV[!duplicated(DSERV), ]
+
+a <- DELIV[duplicated(DELIV$Entrega),]
+a <- a %>% group_by(Centro) %>% mutate(Repeticiones = n())
+a <- a[!duplicated(a$Centro),]
+Plantas <- subset(Plantas, Plantas$Año == 2022 & Plantas$Mes == 12)
+a <- a[,c(1,49)]
+Plantas <- Plantas[,c(3,4,6,7,8)]
+colnames(Plantas) <- c("PD-Code", "Centro", "Nombre de Planta", "Plaza", "Zona")
+Rem_Repetidas <- left_join(a,Plantas)
+
+
+b <- DSERV[duplicated(DSERV$Entrega),]
+b <- b %>% group_by(Centro) %>% mutate(Repeticiones = n())
+a <- a[!duplicated(a$Centro),]
+Plantas <- subset(Plantas, Plantas$Año == 2022 & Plantas$Mes == 12)
+a <- a[,c(1,49)]
+Plantas <- Plantas[,c(3,4,6,7,8)]
+colnames(Plantas) <- c("PD-Code", "Centro", "Nombre de Planta", "Plaza", "Zona")
+Rem_Repetidas <- left_join(a,Plantas)
+
+
+write.xlsx(Rem_Repetidas,"C:/Users/E-JAYAM/Documents/GitHub/Codigos-Cemex/Snowflake/Remisiones_Repetiadas_DELIV.xlsx" )
+
+
+sum(subset(Rem_Repetidas$Repeticiones, Rem_Repetidas$Zona == "CENTRO"))
+sum(subset(Rem_Repetidas$Repeticiones, Rem_Repetidas$Zona == "SURESTE"))
+sum(subset(Rem_Repetidas$Repeticiones, Rem_Repetidas$Zona == "PACIFICO"))
+sum(subset(Rem_Repetidas$Repeticiones, Rem_Repetidas$Zona == "NORESTE"))
+
+
 
 length(unique(DELIV$Entrega))
 
